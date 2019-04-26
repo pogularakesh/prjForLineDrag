78e2594508ddafaa22c04075dd3c8bbc8b4d55f7
rakesh <pogularakesh209@gmail.com>
2019-04-26 16:31:28 +0530

    firstcommit

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import 'jquery-ui-dist/jquery-ui';
+
+
+
+// declare const anchor:any;
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'rilProject4';
+  ngOnInit() {
+    // anchor()
+  }
+  
+  
+}
